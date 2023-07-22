deda9f8fed90411ec0392ec53cacbe623590800c
Glenn Montera <glenn.montera@gmail.com>
2023-07-22 13:01:43 -0400

    add axios api call

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,49 +1,60 @@
-const images = [
-	{
-		url: "./assets/images/Photo-gallery-1.jpg",
-	},
-	{
-		url: "./assets/images/Photo-gallery-2.jpg",
-	},
-	{
-		url: "./assets/images/Photo-gallery-3.jpg",
-	},
-	{
-		url: "./assets/images/Photo-gallery-4.jpg",
-	},
-	{
-		url: "./assets/images/Photo-gallery-5.jpg",
-	},
-	{
-		url: "./assets/images/Photo-gallery-6.jpg",
-	},
-	{
-		url: "./assets/images/Photo-gallery-6.jpg",
-	},
-	{
-		url: "./assets/images/Photo-gallery-6.jpg",
-	},
-	{
-		url: "./assets/images/Photo-gallery-6.jpg",
-	},
+// const images = [
+//   {
+//     url: "./assets/images/Photo-gallery-1.jpg",
+//   },
+//   {
+//     url: "./assets/images/Photo-gallery-2.jpg",
+//   },
+//   {
+//     url: "./assets/images/Photo-gallery-3.jpg",
+//   },
+//   {
+//     url: "./assets/images/Photo-gallery-4.jpg",
+//   },
+//   {
+//     url: "./assets/images/Photo-gallery-5.jpg",
+//   },
+//   {
+//     url: "./assets/images/Photo-gallery-6.jpg",
+//   },
+//   {
+//     url: "./assets/images/Photo-gallery-6.jpg",
+//   },
+//   {
+//     url: "./assets/images/Photo-gallery-6.jpg",
+//   },
+//   {
+//     url: "./assets/images/Photo-gallery-6.jpg",
+//   },
 
-	{
-		url: "./assets/images/Photo-gallery-6.jpg",
-	},
-	{
-		url: "./assets/images/Photo-gallery-6.jpg",
-	},
-];
+//   {
+//     url: "./assets/images/Photo-gallery-6.jpg",
+//   },
+//   {
+//     url: "./assets/images/Photo-gallery-6.jpg",
+//   },
+// ];
 
-const showGallery = () => {
-	images.forEach((image) => {
-		// create
-		createImageContainer(image.url);
-	});
+const key =
+  "live_kT9IUM097S8mkNWl8OxZ4onGT2LTWhVh78WXjCTZE5mRBR5Fu1LCjw9i1zDlDThR";
+
+const pet = axios.get(
+  "https://api.thecatapi.com/v1/images/search?limit=100&api_key=" + key
+);
+pet.then((result) => {
+  console.log(result.data);
+  showGallery(result.data);
+});
+
+const showGallery = (pets) => {
+  pets.forEach((image) => {
+    // create
+    createImageContainer(image.url);
+  });
 };
 
 const createImageContainer = (imgUrl) => {
-	/**
+  /**
 	 * 
 	 * <div class="photo-gallery__item">
 						<img
@@ -54,17 +65,17 @@ const createImageContainer = (imgUrl) => {
 					</div>
 	 * 
 	 */
-	let parentEl = document.querySelector(".photo-gallery");
+  let parentEl = document.querySelector(".photo-gallery");
 
-	let photoGalleryItem = document.createElement("div");
-	photoGalleryItem.classList.add("photo-gallery__item");
+  let photoGalleryItem = document.createElement("div");
+  photoGalleryItem.classList.add("photo-gallery__item");
 
-	let img = document.createElement("img");
-	img.classList.add("photo-gallery__image");
-	img.src = imgUrl;
-	photoGalleryItem.appendChild(img);
-	parentEl.appendChild(photoGalleryItem);
+  let img = document.createElement("img");
+  img.classList.add("photo-gallery__image");
+  img.src = imgUrl;
+  photoGalleryItem.appendChild(img);
+  parentEl.appendChild(photoGalleryItem);
 };
 
 // Onload show gallery
-showGallery();
+// showGallery();
